8d02402c460aa330aa807a94ef25c0d7e9d1d64a
cgildea <cbgildea@gmail.com>
2014-10-19 15:48:20 -0700

    Binary search is not finding the term in the vocab

---
+++ hw5/src/PorterStemmer.java
@@ -0,0 +1,269 @@
+
+import java.util.Collections;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.regex.*;
+
+public class PorterStemmer {
+
+    final static Map<String, String> step1a = createStep1a();
+    final static Map<String, String> step1b = createStep1b();
+    final static Map<String, String> step2 = createStep2();
+    final static Map<String, String> step1bStar = createStep1bStar();
+    final static Map<String, String> step3 = createStep3();
+    final static Map<String, String> step4 = createStep4();
+
+    // a single consonant
+    private static final String c = "[^aeiou]";
+    // a single vowel
+    private static final String v = "[aeiouy]";
+
+    // a sequence of consonants; the second/third/etc consonant cannot be 'y'
+    private static final String C = c + "[^aeiouy]*";
+    // a sequence of vowels; the second/third/etc cannot be 'y'
+    private static final String V = v + "[aeiou]*";
+
+    // this regex pattern tests if the token has measure > 0 [at least one VC].
+    private static final Pattern mGr0 = Pattern.compile("^(" + C + ")?" + "(" + V + C + ")+(" + V + ")?");
+
+    // this regex pattern tests if the token has a measure equal to 1
+    private static final Pattern mEq1 = Pattern.compile(mGr0 + "(" + V + ")?$");
+
+    // this regex pattern tests if the token has a measure greater than 1
+    private static final Pattern mGr1 = Pattern.compile("^(" + C + ")?" + "(" + V + C + V + C + ")+(" + V + ")?");
+
+    // this regrex pattern tests if the token has a vowel after the first (optional) C
+    private static final Pattern hasV = Pattern.compile("(.*)?" + V + "(.*)?");
+
+    // this regrex patten tests if the token ends in a double consonant NOT *L *S *Z
+    private static final Pattern doubleC = Pattern.compile("(.*)?([^aeioulsz])\\2$");
+
+    // this regrx pattern tests if the token has a measure of 1 and the last c is not w, x, or y
+    private static final Pattern cVCNotWXY = Pattern.compile("^(" + C + ")?" + v + "[^aeiouywxy]$");
+
+    public static String processToken(String token) {
+        if (token.length() < 3) {
+            return token; // token must be at least 3 chars
+        }
+        token = step1a(token);
+        token = step1b(token);
+        token = step1c(token);
+        token = step2(token);
+        token = step3(token);
+        token = step4(token);
+        token = step5(token);
+
+        return token;
+    }
+
+    public static String step1a(String token) {
+
+        for (int i = 0; i < step1a.size(); i++) {
+
+            String prefix = (String) step1a.keySet().toArray()[i];
+            String newPrefix = (String) step1a.values().toArray()[i];
+
+            if (token.endsWith(prefix) && !token.endsWith("ss")) {
+                token = token.substring(0, token.length() - prefix.length()) + newPrefix;
+                break;
+            }
+        }
+        return token;
+    }
+
+    public static String step1b(String token) {
+
+        for (int i = 0; i < step1b.size(); i++) {
+
+            String suffix = (String) step1b.keySet().toArray()[i];
+            String newSuffix = (String) step1b.values().toArray()[i];
+            Pattern[] patterns = {mGr0, hasV, hasV};
+
+            if (token.endsWith(suffix)) {
+                if (patterns[i].matcher(token.substring(0, token.length() - suffix.length())).find()) {
+                    token = token.substring(0, token.length() - suffix.length()) + newSuffix;
+                    if (i > 0) {
+                        token = step1bStar(token);
+                    }
+                }
+                break;
+            }
+        }
+        return token;
+    }
+
+    public static String step1bStar(String token) {
+
+        for (int i = 0; i < step1bStar.size(); i++) {
+
+            String suffix = (String) step1bStar.keySet().toArray()[i];
+            String newSuffix = (String) step1bStar.values().toArray()[i];
+
+            if (token.endsWith(suffix)) {
+                token = token.substring(0, token.length() - suffix.length()) + newSuffix;
+                return token;
+            }
+        }
+        if (doubleC.matcher(token).find()) {
+            token = token.substring(0, token.length() - 1);
+        } else if (mEq1.matcher(token).find() && cVCNotWXY.matcher(token).find()) {
+            token += "e";
+        }
+        return token;
+    }
+
+    public static String step1c(String token) {
+        if (token.endsWith("y") && hasV.matcher(token).find()) {
+            token = token.substring(0, token.length() - 1) + "i";
+        }
+        return token;
+    }
+
+    public static String step2(String token) {
+
+        for (int i = 0; i < step2.size(); i++) {
+
+            String suffix = (String) step2.keySet().toArray()[i];
+            String newSuffix = (String) step2.values().toArray()[i];
+
+            if (token.endsWith(suffix)) {
+                if (mGr0.matcher(token.substring(0, token.length() - suffix.length())).find()) {
+                    token = token.substring(0, token.length() - suffix.length()) + newSuffix;
+                }
+                break;
+            }
+        }
+        return token;
+    }
+
+    public static String step3(String token) {
+
+        for (int i = 0; i < step3.size(); i++) {
+
+            String suffix = (String) step3.keySet().toArray()[i];
+            String newSuffix = (String) step3.values().toArray()[i];
+
+            if (token.endsWith(suffix)) {
+                if (mGr0.matcher(token.substring(0, token.length() - suffix.length())).find()) {
+                    token = token.substring(0, token.length() - suffix.length()) + newSuffix;
+                }
+                break;
+            }
+        }
+        return token;
+    }
+
+    public static String step4(String token) {
+
+        for (int i = 0; i < step4.size(); i++) {
+
+            String suffix = (String) step4.keySet().toArray()[i];
+            String newSuffix = (String) step4.values().toArray()[i];
+
+            if (token.endsWith(suffix)) {
+                if (mGr1.matcher(token.substring(0, token.length() - suffix.length())).find()) {
+                    token = token.substring(0, token.length() - suffix.length()) + newSuffix;
+                }
+                break;
+            }
+        }
+        return token;
+    }
+
+    public static String step5(String token) {
+
+        if (token.endsWith("e") && mGr1.matcher(token.substring(0, token.length() - 1)).find()) {
+            return token.substring(0, token.length() - 1);
+        } else if (token.endsWith("e") && mEq1.matcher(token.substring(0, token.length() - 1)).find()
+                && cVCNotWXY.matcher(token.substring(0, token.length() - 1)).find()) {
+            return token.substring(0, token.length() - 1);
+        } else if (token.endsWith("ll") && mGr1.matcher(token.substring(0, token.length() - 1)).find()) {
+            return token.substring(0, token.length() - 1);
+        }
+        return token;
+    }
+
+    private static Map<String, String> createStep1a() {
+        Map<String, String> stem = new LinkedHashMap<>();
+        stem.put("sses", "ss");
+        stem.put("ies", "i");
+        stem.put("s", "");
+        return Collections.unmodifiableMap(stem);
+    }
+
+    private static Map<String, String> createStep1b() {
+        Map<String, String> stem = new LinkedHashMap<>();
+        stem.put("eed", "ee");
+        stem.put("ed", "");
+        stem.put("ing", "");
+        return Collections.unmodifiableMap(stem);
+    }
+
+    private static Map<String, String> createStep1bStar() {
+        Map<String, String> stem = new LinkedHashMap<>();
+        stem.put("at", "ate");
+        stem.put("bl", "ble");
+        stem.put("iz", "ize");
+        return Collections.unmodifiableMap(stem);
+    }
+
+    private static Map<String, String> createStep2() {
+        Map<String, String> stem = new LinkedHashMap<>();
+        stem.put("ational", "ate");
+        stem.put("iveness", "ive");
+        stem.put("fulness", "ful");
+        stem.put("ization", "ize");
+        stem.put("ousness", "ous");
+        stem.put("tional", "tion");
+        stem.put("biliti", "ble");
+        stem.put("entli", "ent");
+        stem.put("ousli", "ous");
+        stem.put("ation", "ate");
+        stem.put("alism", "al");
+        stem.put("aviti", "ive");
+        stem.put("aliti", "al");
+        stem.put("enci", "ence");
+        stem.put("anci", "ance");
+        stem.put("izer", "ize");
+        stem.put("alli", "al");
+        stem.put("bli", "ble");
+        stem.put("eli", "e");
+        return Collections.unmodifiableMap(stem);
+    }
+
+    private static Map<String, String> createStep3() {
+        Map<String, String> stem = new LinkedHashMap<>();
+        stem.put("icate", "ic");
+        stem.put("ative", "");
+        stem.put("alize", "al");
+        stem.put("iciti", "ic");
+        stem.put("ical", "ic");
+        stem.put("ness", "");
+        stem.put("ful", "");
+        return Collections.unmodifiableMap(stem);
+    }
+
+    private static Map<String, String> createStep4() {
+        Map<String, String> stem = new LinkedHashMap<>();
+        stem.put("ement", "");
+        stem.put("tion", "t");
+        stem.put("sion", "s");
+        stem.put("ible", "");
+        stem.put("able", "");
+        stem.put("ence", "");
+        stem.put("ment", "");
+        stem.put("ance", "");
+        stem.put("ant", "");
+        stem.put("ent", "");
+        stem.put("ism", "");
+        stem.put("ate", "");
+        stem.put("iti", "");
+        stem.put("ous", "");
+        stem.put("ive", "");
+        stem.put("ize", "");
+        stem.put("al", "");
+        stem.put("er", "");
+        stem.put("ou", "");
+        return Collections.unmodifiableMap(stem);
+    }
+}
---
+++ hw5/src/SimpleEngine.java
@@ -0,0 +1,121 @@
+
+import java.io.*;
+import java.nio.file.*;
+import java.nio.file.attribute.*;
+import java.util.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * A very simple search engine. Uses an inverted index over a folder of TXT
+ * files.
+ */
+public class SimpleEngine {
+
+    public static void main(String[] args) throws IOException {
+        final Path currentWorkingPath = Paths.get("").toAbsolutePath();
+
+        // the inverted index
+        final NaiveInvertedIndex index = new NaiveInvertedIndex();
+
+        // the list of file names that were processed
+        final List<String> fileNames = new ArrayList<String>();
+       
+        
+        String porter;
+
+        // This is our standard "walk through all .txt files" code.
+        Files.walkFileTree(currentWorkingPath, new SimpleFileVisitor<Path>() {
+            int mDocumentID = 0;
+
+            public FileVisitResult preVisitDirectory(Path dir,
+                    BasicFileAttributes attrs) {
+                // make sure we only process the current working directory
+                if (currentWorkingPath.equals(dir)) {
+                    return FileVisitResult.CONTINUE;
+                }
+                return FileVisitResult.SKIP_SUBTREE;
+            }
+
+            public FileVisitResult visitFile(Path file,
+                    BasicFileAttributes attrs) {
+                // only process .txt files
+                if (file.toString().endsWith(".txt")) {
+               // we have found a .txt file; add its name to the fileName list,
+                    // then index the file and increase the document ID counter.
+                    //System.out.println("Indexing file " + file.getFileName());
+
+                    fileNames.add(file.getFileName().toString());
+                    indexFile(file.toFile(), index, mDocumentID);
+                    mDocumentID++;
+                }
+                return FileVisitResult.CONTINUE;
+            }
+
+            // don't throw exceptions if files are locked/other errors occur
+            public FileVisitResult visitFileFailed(Path file,
+                    IOException e) {
+
+                return FileVisitResult.CONTINUE;
+            }
+
+        });
+
+        printResults(index, fileNames);
+
+        Scanner scan = new Scanner(System.in);
+
+        String searchedWord = "";
+        while (!"quit".equalsIgnoreCase(searchedWord)) {
+            System.out.print("\nEnter a word to search: ");
+            searchedWord = scan.next();
+            if ("quit".equalsIgnoreCase(searchedWord)) {
+                break;
+            }
+            List<String> documentNames = new ArrayList<>();
+
+            porter = PorterStemmer.processToken(searchedWord);
+            documentNames = index.getContainingDocuments(porter, fileNames);
+            if(documentNames.isEmpty())
+                System.out.println("The word \""+ searchedWord + "\" was not found.");        
+            else
+                System.out.println("Word is found in: "+ documentNames.toString());
+        }
+        scan.close();
+    }
+
+    /**
+     * Indexes a file by reading a series of tokens from the file, treating each
+     * token as a term, and then adding the given document's ID to the inverted
+     * index for the term.
+     *
+     * @param file a File object for the document to index.
+     * @param index the current state of the index for the files that have
+     * already been processed.
+     * @param docID the integer ID of the current document, needed when indexing
+     * each term from the document.
+     */
+    private static void indexFile(File file, NaiveInvertedIndex index,
+            int docID) {
+        try {
+            SimpleTokenStream tokenStream = new SimpleTokenStream(file);
+            while(tokenStream.hasNextToken()){
+                index.addTerm(PorterStemmer.processToken(tokenStream.nextToken()), docID);
+            }
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(SimpleEngine.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    private static void printResults(NaiveInvertedIndex index,
+            List<String> fileNames) {
+        int longestWord = 0;
+        for (String dict : index.getDictionary()) {
+            longestWord = Math.max(longestWord, dict.length());
+        }
+        for(String token : index.getDictionary()){
+            System.out.format("%-" + longestWord + "s: %s\n", token, index.getContainingDocuments(token, fileNames).toString());
+        }
+       
+    }
+}
